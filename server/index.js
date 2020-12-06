c649f09a280bee33cd522635e2336bc1abdf276b
TMB <Troy.Boettger@gmail.com>
2020-12-06 04:17:22 -0500

    implement functions to deal with users

--- server/index.js
+++ server/index.js
@@ -16,6 +16,10 @@ const io = socketio(server)
 io.on('connection', (socket) => {
 	console.log('we have a new connection!!! WOOHOOOO!')
 
+	socket.on('join', ({ name, room }, callback) => {
+		console.log(name, room)
+	})
+
 	socket.on('disconnect', () => {
 		console.log('User left the connection!')
 	})
