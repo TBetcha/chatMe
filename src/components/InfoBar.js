552d6be4db2a0380780366f5f4a82307920cad27
TMB <Troy.Boettger@gmail.com>
2020-12-13 05:48:02 -0500

    build infobar component

---
+++ src/components/InfoBar.js
@@ -0,0 +1,24 @@
+/** @format */
+
+import React from 'react'
+import '../css/InfoBar.css'
+import closeIcon from '../icons/closeIcon.png'
+import onlineIcon from '../icons/onlineIcon.svg'
+
+const InfoBar = ({ room }) => {
+	return (
+		<div className='infoBar'>
+			<div className='leftInnerContainer'>
+				<img className='onlineIcon' src={onlineIcon} alt='online image' />
+				<h3>{room}</h3>
+			</div>
+			<div className='rightInnerContainer'>
+				<a href='/'>
+					<img src={closeIcon} alt='close image' />
+				</a>
+			</div>
+		</div>
+	)
+}
+
+export default InfoBar
