bf94278539c8e2d908adf406d4e33ffdc96f7364
TMB <Troy.Boettger@gmail.com>
2020-12-16 07:29:33 -0500

    can now display sent messages in chat screen

--- server/index.js
+++ server/index.js
@@ -31,18 +31,25 @@ io.on('connection', (socket) => {
 
 		socket.join(user.room)
 
+		io.to(user.room).emit('roomData', { room: user.room, users: getUserInRoom(user.room) })
+
 		callback()
 	})
 
 	socket.on('sendMessage', (message, callback) => {
 		const user = getUser(socket.id)
 		io.to(user.room).emit('message', { user: user.name, text: message })
+		io.to(user.room).emit('roomData', { room: user.room, users: getUserInRoom(user.room) })
 
 		callback()
 	})
 
 	socket.on('disconnect', () => {
 		console.log('User left the connection!')
+		const user = removeUser(socket.id)
+		if (user) {
+			io.to(user.room).emit('message', { user: 'admin', text: `${user.name} has left.` })
+		}
 	})
 })
 
